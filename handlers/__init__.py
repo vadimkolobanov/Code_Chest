c2a8cd7555f8450cfd8d8ccae9342587d3c8cc6e
vadim <titanyforgame@gmail.com>
2021-12-15 17:19:23 +0300

    поддержка проекта кнопка и  меню

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 from handlers import client
 from handlers import admin
 from handlers import moderator_panel
+from handlers import monetize
 from handlers import other
--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,2 +1,3 @@
 from keyboards.client_kb import kb_client
 from keyboards.admin_kb import kb_lvl, kb_lang, kb_action, kb_inline_del
+from keyboards.donate_kb import kb_donate
